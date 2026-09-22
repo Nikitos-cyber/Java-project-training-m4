19664a3be388215883cc064aa4f62c599393f2ae
ngolomazdin <golomazdin1212@gmail.com>
2026-09-22 09:47:22 +0300

    First version Generation str

---
+++ src/GitTest/GitTest.java
@@ -0,0 +1,38 @@
+package GitTest;
+
+
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GitTest {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Random rand = new Random();
+
+        StringBuilder str = new StringBuilder();
+
+        System.out.print("Введите длинну генерации строки: ");
+
+        int length = 0;
+        if(scanner.hasNextInt())
+          length  = scanner.nextInt();
+        else return;
+
+        for(int i =0; i < length; ++i){
+            int numberRand = rand.nextInt(26);
+            int randCase = rand.nextInt(10);
+            int symbolInt = 'a' + numberRand;
+            if(randCase > 5)
+              str.append(String.valueOf((char) symbolInt).toUpperCase(Locale.ROOT));
+            else
+                str.append(String.valueOf((char) symbolInt));
+        }
+
+        System.out.println("\nСтрока: " + str.toString());
+
+    }
+
+}
---
+++ src/HomeWork/Main.java
@@ -0,0 +1,10 @@
+package HomeWork;
+
+public class Main {
+    public static  void main(String[] args){
+
+
+
+    }
+
+}
