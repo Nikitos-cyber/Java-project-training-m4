c3b867146934c79015ab715b14eaab34cff5f20f
ngolomazdin <golomazdin1212@gmail.com>
2026-09-22 11:10:50 +0300

    array show

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         int [] arr = {23,4,5,5,10};
         java.util.Arrays.sort(arr);
-        System.out.println(java.util.Arrays.toString(arr));
+        System.out.println("Array:" + java.util.Arrays.toString(arr));
     }
 }
